404e9311ede701daf77b93d04356de9e6df1efd8
Ali Talebi <talebia1993@gmail.com>
2022-04-19 23:36:06 +0430

    fixd book tools

--- resources/js/Pages/Pages/Index.vue
+++ resources/js/Pages/Pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container flex flex-col justify-start items-center min-h-screen py-2 gap-4">
+    <div class="flex flex-col justify-start items-center max-h-full min-h-full py-2 gap-4">
         <div class="modal" :class="{'modal-open': isOpenModalSetPermission}">
             <div class="modal-box overflow-visible  w-fit max-w-5xl relative">
                 <label class="btn btn-sm btn-circle absolute -right-2 -top-2" @click="isOpenModalSetPermission = false">✕</label>
@@ -82,47 +82,49 @@
                 </Link>
             </div>
         </div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-2">
-            <div class="card bg-base-100 shadow-xl image-full card-bordered border-gray-300 bg-base-100 shadow-xl" v-for="page in book.pages" :key="`page-${page.id}`">
-                <figure><img :src="page.imageThumbnail300Url"  :alt="page.number"/></figure>
-                <div class="card-body justify-between items-center">
-                    <div class="card-actions flex justify-between items-center w-full">
-                        <div class="dropdown">
-                            <label tabindex="0" class="btn btn-xs m-1">
-                                <i class="fa" :class="{
+        <div class="flex-grow max-h-full overflow-auto">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-2 pb-16">
+                <div class="card bg-base-100 shadow-xl image-full card-bordered border-gray-300 bg-base-100 shadow-xl" v-for="page in book.pages" :key="`page-${page.id}`">
+                        <figure><img :src="page.imageThumbnail300Url"  :alt="page.number"/></figure>
+                        <div class="card-body justify-between items-center">
+                            <div class="card-actions flex justify-between items-center w-full">
+                                <div class="dropdown">
+                                    <label tabindex="0" class="btn btn-xs m-1">
+                                        <i class="fa" :class="{
                                     'fa-circle': page.status === 'INITIAL',
                                     'fa-clock': page.status === 'PENDING',
                                     'fa-check': page.status === 'DONE',
                                     'fa-check-double': page.status === 'APPROVED',
                                 }"></i>
-                            </label>
-                            <div tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box gap-1" v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')">
-                                <button v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')" class="btn btn-xs" @click="()=>setStatus(page,'INITIAL')">
-                                    <i class="fa fa-circle"></i>
-                                </button>
-                                <button v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')" class="btn btn-xs" @click="()=>setStatus(page,'PENDING')">
-                                    <i class="fa fa-clock"></i>
-                                </button>
-                                <button v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')" class="btn btn-xs" @click="()=>setStatus(page,'DONE')">
-                                    <i class="fa fa-check"></i>
-                                </button>
-                                <button v-if="hasRole('Super-Admin')" class="btn btn-xs" @click="()=>setStatus(page,'APPROVED')">
-                                    <i class="fa fa-check-double"></i>
-                                </button>
+                                    </label>
+                                    <div tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box gap-1" v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')">
+                                        <button v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')" class="btn btn-xs" @click="()=>setStatus(page,'INITIAL')">
+                                            <i class="fa fa-circle"></i>
+                                        </button>
+                                        <button v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')" class="btn btn-xs" @click="()=>setStatus(page,'PENDING')">
+                                            <i class="fa fa-clock"></i>
+                                        </button>
+                                        <button v-if="hasRole('Super-Admin') || (can('pages.update', page.id) &&  page.status !== 'APPROVED')" class="btn btn-xs" @click="()=>setStatus(page,'DONE')">
+                                            <i class="fa fa-check"></i>
+                                        </button>
+                                        <button v-if="hasRole('Super-Admin')" class="btn btn-xs" @click="()=>setStatus(page,'APPROVED')">
+                                            <i class="fa fa-check-double"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <input type="checkbox" class="checkbox checkbox-sm checkbox-accent" :value="page.id" v-model="checkedPages">
+                            </div>
+                            <h2 class="card-title">{{ page.number }}</h2>
+                            <div class="card-actions justify-center flex-nowrap justify-evenly">
+                                <Link v-if="can('pages.read', page.id)" :href="$route('books.pages.show', [book.id, page.id])" class="btn btn-primary">
+                                    <i class="fas fa-edit"></i>
+                                </Link>
+                                <Link  v-if="can('pages.destroy', page.id)" as="button" :href="$route('books.pages.destroy', [book.id, page.id])" method="delete" class="btn btn-error">
+                                    <i class="fas fa-trash-alt"></i>
+                                </Link>
                             </div>
                         </div>
-                        <input type="checkbox" class="checkbox checkbox-sm checkbox-accent" :value="page.id" v-model="checkedPages">
-                    </div>
-                    <h2 class="card-title">{{ page.number }}</h2>
-                    <div class="card-actions justify-center flex-nowrap justify-evenly">
-                        <Link v-if="can('pages.read', page.id)" :href="$route('books.pages.show', [book.id, page.id])" class="btn btn-primary">
-                            <i class="fas fa-edit"></i>
-                        </Link>
-                        <Link  v-if="can('pages.destroy', page.id)" as="button" :href="$route('books.pages.destroy', [book.id, page.id])" method="delete" class="btn btn-error">
-                            <i class="fas fa-trash-alt"></i>
-                        </Link>
-                    </div>
-                </div>
+            </div>
             </div>
         </div>
     </div>
