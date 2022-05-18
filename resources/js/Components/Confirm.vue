c01b8b7b4040b77a8dec977ed1abcf37eb5f6c35
Ali Talebi <talebia1993@gmail.com>
2022-05-18 22:57:40 +0430

    create confirm component

---
+++ resources/js/Components/Confirm.vue
@@ -0,0 +1,48 @@
+<template>
+    <slot :open="open"></slot>
+    <div v-if="isOpen" class="modal modal-bottom sm:modal-middle" :class="{'modal-open': isOpen}">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Are you sure?</h3>
+            <div class="modal-action">
+                <button type="button" class="btn" @click="confirm">Confirm</button>
+                <button type="button" class="btn" @click="cancel">Cancel</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Confirm",
+    props:{
+        confirmAction: {
+            type: Function,
+            required: true
+        },
+    },
+    data() {
+        return {
+            isOpen: false,
+        }
+    },
+    methods: {
+        open() {
+            this.isOpen = true;
+        },
+        close() {
+            this.isOpen = false;
+        },
+        confirm() {
+            this.confirmAction();
+            this.close();
+        },
+        cancel() {
+            this.close();
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
